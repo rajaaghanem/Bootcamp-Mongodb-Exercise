cdcc154a564c7c1103d5f7542e231ac5e853cd87
rajaaghanem <rajaaghanem@gmail.com>
2022-01-19 11:59:04 +0200

    Get products with a specific price range

--- 18.1-create and read/src/index.js
+++ 18.1-create and read/src/index.js
@@ -32,22 +32,43 @@ app.get("/products", (req, res) => {
     });
 });
 
+//Get products that are active
+app.get("/products/active", (req, res) => {
+    Product.find({ isActive: true })
+      .then((products) => {
+        res.status(200).send(products);
+      })
+      .catch((e) => {
+        res.status(400).send(e);
+      });
+  });
+
+//Get products with a specific price range
+app.get("/products/range", (req, res) => {
+    console.log(req.query);
+    Product.find({$and:[{"details.Price": {$lt: req.query.max}},{"details.Price": {$gt: req.query.min}}]})
+      .then((products) => {
+        res.status(200).send(products);
+      })
+      .catch((e) => {
+        res.status(400).send(e);
+      });
+  });
 
 //Get a specific product by id
 app.get("/products/:id", handleResponse);
 
 function handleResponse(req, res) {
+  const _id = req.params.id;
 
-    const _id = req.params.id;
-
-    Product.findById(_id)
-        .then((product) => {
-            if (!product) return res.status(404).send();
-            res.status(200).send(product);
-        })
-        .catch((e) => {
-            res.status(400).send(e.message);
-        });
+  Product.findById(_id)
+    .then((product) => {
+      if (!product) return res.status(404).send();
+      res.status(200).send(product);
+    })
+    .catch((e) => {
+      res.status(400).send(e.message);
+    });
 }
 
 
