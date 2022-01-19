fc2e2ed769229d75a3e8dcc134686faa7b2c39c8
rajaaghanem <rajaaghanem@gmail.com>
2022-01-19 16:07:01 +0200

    invalid updates handle

--- 20.1-update and delete/src/index.js
+++ 20.1-update and delete/src/index.js
@@ -71,6 +71,15 @@ async function handleResponse(req, res) {
 //update a product to become active/not active and change the value of its discount.
 app.patch("/products/active/:id", async (req, res) => {
   const _id = req.params.id;
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['isActive', 'details.discount'];
+  const isValidOperation = updates.every((update)=>allowedUpdates.includes(update));
+
+  if (!isValidOperation){
+    return res.status(400).send ({error : 'invalid updates'});
+  }
+
   try {
     const editedProduct = await Product.findByIdAndUpdate(_id, req.body, {
       new: true,
@@ -79,11 +88,13 @@ app.patch("/products/active/:id", async (req, res) => {
 
     if (!editedProduct) return res.status(404).send();
     res.status(200).send(editedProduct);
-
   } catch (e) {
     res.status(400).send(e.message);
   }
 });
+
+// Delete a specific product
+
 
 app.listen(port, () => {
   console.log("Server is up on port" + port);
