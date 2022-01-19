8ef8154a500c938be0f2ea4d8047698d8c8b1a30
rajaaghanem <rajaaghanem@gmail.com>
2022-01-19 09:45:46 +0200

    used validator and deleted the schema

--- 17.1-product_model/src/db/mongoose.js
+++ 17.1-product_model/src/db/mongoose.js
@@ -1,46 +1,15 @@
 const { LogarithmicScale } = require("chart.js");
 const mongoose = require("mongoose");
+const validator = require("validator");
 const { Schema } = mongoose;
 const url = "mongodb://localhost:27017/mongoose";
 
-// { useUnifiedTopology: true, useCreateIndex: true }
 mongoose.connect(url, (error, client) => {
   if (error) {
     return console.log("unable to connect to database");
   }
   console.log("connected correctly");
 });
-
-const proudctDetails = new Schema({
-  description: {
-    type: String,
-    required: true,
-    minLength: 10,
-  },
-  Price: {
-    type: Number,
-    validate(value) {
-      if (value < 0) throw new Error("Price most be positive number");
-    },
-    required: true,
-  },
-  discount: {
-    type: Number,
-    default: 0,
-  },
-  array: {
-    type: Array,
-    minLength: 2,
-  },
-  phone: {
-    type: String,
-    required: true,
-    minLength: 10,
-    maxLength: 10,
-  },
-});
-
-const Details = mongoose.model("details", proudctDetails);
 
 const Product = mongoose.model("Product", {
   name: {
@@ -55,53 +24,62 @@ const Product = mongoose.model("Product", {
   isActive: {
     type: Boolean,
   },
-  details: [proudctDetails],
+
+  details: {
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+    },
+    Price: {
+      type: Number,
+      validate(value) {
+        if (value < 0) throw new Error("Price most be positive number");
+      },
+      required: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    array: {
+      type: [String],
+      minItems: 2,
+    },
+    phone: {
+      type: String,
+      validate(value){
+          if (!validator.isMobilePhone(value, 'he-IL'))
+          throw Error ('not valid phone number');
+      },
+      required: true,
+    },
+    date:
+    {
+        type: Date,
+        default: Date.now()
+    }
+  },
 });
 
-// const firstProductDetails = new Details({
-//   description: "a very stylish blue hat",
-//   Price: 100,
-//   discount: 20,
-//   array: ["blue", "hat"],
-//   phone: "0533333333",
-// });
-
-// const firstProduct = new Product({
-//   name: "Blue hat",
-//   category: "clothes",
-//   isActive: true,
-//   details: firstProductDetails,
-// });
-
-// firstProduct
-//   .save()
-//   .then(() => {
-//     console.log(firstProduct);
-//   })
-//   .catch((error) => {
-//     console.log("error");
-//   });
-
-const secoundProductDetails = new Details({
-    description: "plus size red dress",
-    Price: 30,
-    discount: 0,
-    array: ["red", "plus"],
-    phone: "0533333334",
-  });
-
-const secoundProduct = new Product({
-  name: "Red dress",
+const thirdProduct = new Product({
+  name: "test",
   category: "clothes",
   isActive: true,
-  details: secoundProductDetails,
+  details: {
+    description: "plus size purple dress",
+    Price: 30,
+    discount: 0,
+    array: ["dress", "plus"],
+    phone: "0539999999",
+  },
 });
 
-secoundProduct
+thirdProduct
   .save()
   .then(() => {
-    console.log(secoundProduct);
+    console.log(thirdProduct);
   })
   .catch((error) => {
-    console.log("error");
+    console.log("error, one of the fileds is not correct");
   });
